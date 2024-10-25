5ee7a267442c78185eec645805471245fba3f8d2
Sushil-Jadhav07 <sushiljadhav407@gmail.com>
2024-10-25 18:35:04 +0530

    first-change

--- app/page.js
+++ app/page.js
@@ -1,50 +1,27 @@
+
 import Layout from "@/components/layout/Layout"
-import About from "@/components/sections/home1/About"
-import Banner from "@/components/sections/home1/Banner"
-import Feature from "@/components/sections/home1/Feature"
-import Funfacts from "@/components/sections/home1/Funfacts"
-import WhyChooseUs from "@/components/sections/home1/WhyChooseUs"
-import Service from "@/components/sections/home1/Service"
-import Video from "@/components/sections/home1/video"
-import Brand from "@/components/sections/home1/Brand"
-import Blog from "@/components/sections/home1/Blog"
-import Faq from "@/components/sections/home1/Faq"
-import Quote from "@/components/sections/home1/Quote"
-import Testimonial from "@/components/sections/home1/Testimonial"
-import Cta from "@/components/sections/home1/Cta"
-import Portfolio from "@/components/sections/home1/Portfolio"
-import Pricing from "@/components/sections/home1/Pricing"
-import Team from "@/components/sections/home1/Team"
-import News from "@/components/sections/home1/News"
-import Appointment from "@/components/sections/home1/Appointment"
-import Features from "@/components/sections/home1/Features"
+
+import About from "@/components/sections/home4/About"
+import Banner from "@/components/sections/home4/Banner"
+import Blog from "@/components/sections/home4/Blog"
+import Features from "@/components/sections/home4/Features"
+import History from "@/components/sections/home4/History"
+import Industries from "@/components/sections/home4/Industries"
+import Team from "@/components/sections/home4/Team"
+import Services from "@/components/sections/home4/Services"
+
 
 export default function Home() {
 
     return (
         <>
-            <Layout headerStyle={1} footerStyle={1}>
+            <Layout headerStyle={4} footerStyle={3}>
                 <Banner />
-                <Features />
                 <About />
-                <Service />
-                <Cta />
-                <WhyChooseUs />
-                <Portfolio />
-                <Funfacts />
-                <Pricing />
-                <Testimonial />
-                <Team />
-                <Faq />
-                <News />
-                <Appointment />
-                <Brand />
-              
-                
-               
-                
+                <Features />
+                <Industries />
+                <Services />
             </Layout>
-
         </>
     )
 }
